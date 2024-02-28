9739182ebeba3d8e0c1edc56478991f02b62e06d
KB-EST <suleimankb2580@gmail.com>
2024-02-28 13:16:29 +0100

    hackathon

---
+++ src/main/java/org/lamisplus/modules/KP_PREV/controller/exception/AlreadyExistException.java
@@ -0,0 +1,9 @@
+package org.lamisplus.modules.KP_PREV.controller.exception;
+
+public class AlreadyExistException extends RuntimeException {
+
+    public AlreadyExistException(String message) {
+        super (message);
+    }
+
+}
---
+++ src/main/java/org/lamisplus/modules/KP_PREV/controller/exception/NoRecordFoundException.java
@@ -0,0 +1,7 @@
+package org.lamisplus.modules.KP_PREV.controller.exception;
+
+public class NoRecordFoundException extends RuntimeException {
+    public NoRecordFoundException(String message) {
+        super (message);
+    }
+}
